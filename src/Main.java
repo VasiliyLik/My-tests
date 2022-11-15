5e5640d8b216f565c6ff5770f2f75c79c86d3712
VasiliyLik <vasya_li@mail.ru>
2022-11-15 22:19:47 +0300

    correct code for testing a new branch

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello java!");
         System.out.println("My test");
+
+        System.out.println("проверка новой ветки");
     }
 }
