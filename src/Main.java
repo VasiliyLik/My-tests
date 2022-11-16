4648e8c116967a440725f0d99419117aff724cde
VasiliyLik <vasya_li@mail.ru>
2022-11-16 16:34:47 +0300

    one more commit for branch

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello java!");
         System.out.println("My test");
 
-        System.out.println("проверка новой ветки");
+        System.out.println("Проверка новой ветки");
+
+        System.out.println();
+
+        int[] array = {1, 3, 9, -4, 8};
+        System.out.println(Arrays.toString(array));
     }
 }
